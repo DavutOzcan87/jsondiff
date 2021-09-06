3446d37de089b25aef21567efb11510e653921de
Davut Ozcan <davutozcan87@gmail.com>
2021-09-06 22:29:05 +0300

    small components added

---
+++ src/components/EditorComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="editor">
+      Editor component
+  </div>
+</template>
+
+<script>
+export default {
+    name:"EditorComponent"
+}
+</script>
+
+<style scoped>
+    .editor{
+        background: red;
+    }
+</style>>
