ffa86f5c1a058b4177c7422e4785c7d32c358896
Your Name <davutozcan87@gmail.com>
2021-09-29 22:51:14 +0300

    objects primitive line number added

--- src/components/jsonnode.js
+++ src/components/jsonnode.js
@@ -1,46 +1,81 @@
 class JsonArray {}
 
-class JsonObject {}
+class JsonObject {
+    constructor(key) {
+        this.key = key;
+        this.children = [];
+        this.dimension = new Dimension(0, 0, 0, 0);
+        this.type = "object";
+    }
+
+    addChildren(parsed) {
+        this.children.push(parsed);
+    }
+}
 
 class JsonPrimitive {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
-    let length = JSON.stringify(value).length;
-    this.dimension = new Dimension(1, 1, 1, length + 1);
-    this.children = [];
-    this.type = "primitive";
-  }
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        let length = JSON.stringify(value).length;
+        this.dimension = new Dimension(1, 1, 1, length + 1);
+        this.children = [];
+        this.type = "primitive";
+    }
+
+    shift(lineNumber) {
+        this.dimension.startLineNumber += lineNumber;
+        this.dimension.endLineNumber += lineNumber;
+    }
 }
 
 class Dimension {
-  constructor(startLineNumber, startColumn, endLineNumber, endColumn) {
-    this.startLineNumber = startLineNumber;
-    this.startColumn = startColumn;
-    this.endLineNumber = endLineNumber;
-    this.endColumn = endColumn;
-  }
+    constructor(startLineNumber, startColumn, endLineNumber, endColumn) {
+        this.startLineNumber = startLineNumber;
+        this.startColumn = startColumn;
+        this.endLineNumber = endLineNumber;
+        this.endColumn = endColumn;
+    }
 }
 
 const parse = function(obj) {
-  console.log("parse object called", obj);
-  if (obj === null || obj == undefined) return EMPTY;
-
-  if (typeof obj === "object") {
-  } else if (Array.isArray(obj)) {
-    return EMPTY;
-  } else {
-    return parsePrimitive(obj);
-  }
+    return parseInternal(obj, 0);
 };
 
-function parsePrimitive(primitive) {
-  return new JsonPrimitive("", primitive);
+const parseInternal = function(obj, startLine) {
+    console.log("parse object called", obj);
+    if (obj === null || obj == undefined) {
+        return EMPTY;
+    }
+
+    if (typeof obj === "object") {
+        return parseOnject(obj, startLine);
+    } else if (Array.isArray(obj)) {
+        return EMPTY;
+    } else {
+        return parsePrimitive(obj, startLine);
+    }
+};
+
+function parseOnject(obj, startLine) {
+    let result = new JsonObject("");
+    let currentLine = startLine + 1;
+    for (let [key, value] of Object.entries(obj)) {
+        result.addChildren(parseInternal(value, currentLine));
+        currentLine++;
+    }
+    return result;
+}
+
+function parsePrimitive(primitive, startLineNumber) {
+    let result = new JsonPrimitive("", primitive);
+    result.shift(startLineNumber);
+    return result;
 }
 
 const EMPTY = {
-  dimensin: new Dimension(0, 0, 0, 0),
-  children: [],
+    dimensin: new Dimension(0, 0, 0, 0),
+    children: []
 };
 
 export { parse };
--- test/unit/jsonNode.spec.js
+++ test/unit/jsonNode.spec.js
@@ -1,32 +1,45 @@
 import { parse } from "../../src/components/jsonnode";
 
+function dim(startLineNumber, startColumn, endLineNumber, endColumn) {
+    return { startLineNumber, startColumn, endLineNumber, endColumn };
+}
+
 test("should parse flat object", () => {
-  const sample = {
-    name: "test",
-    age: 25,
-  };
-  let parsed = parse(sample);
-  expect(parsed).toMatchObject({
-    dimension: {
-      startLineNumber: 1,
-      startColumn: 1,
-      endLineNumber: 4,
-      endColumn: 17,
-    },
-  });
+    const sample = {
+        name: "test",
+        age: 25
+    };
+    let parsed = parse(sample);
+    expect(parsed).toMatchObject({
+        type: "object",
+        // dimension: {
+        //     startLineNumber: 1,
+        //     startColumn: 1,
+        //     endLineNumber: 4,
+        //     endColumn: 17
+        // },
+        children: [
+            {
+                dimension: dim(2, 4, 2, 19)
+            },
+            {
+                dimension: dim(3, 4, 3, 14)
+            }
+        ]
+    });
 });
 
 test("should parse primitive object", () => {
-  const sample = 25;
-  let parsed = parse(sample);
-  expect(parsed).toMatchObject({
-    dimension: {
-      startLineNumber: 1,
-      startColumn: 1,
-      endLineNumber: 1,
-      endColumn: 3,
-    },
-    children: [],
-    type: "primitive",
-  });
+    const sample = 25;
+    let parsed = parse(sample);
+    expect(parsed).toMatchObject({
+        dimension: {
+            startLineNumber: 1,
+            startColumn: 1,
+            endLineNumber: 1,
+            endColumn: 3
+        },
+        children: [],
+        type: "primitive"
+    });
 });
