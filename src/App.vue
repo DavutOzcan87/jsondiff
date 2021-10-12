42c92f14b449daf596a3395254d2ae86ef8ada2c
davut.ozcan <davut.ozcan@accenture.com>
2021-10-13 01:04:57 +0300

    small color enhancement

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <MainComponent msg="Welcome to Your Vue.js App" />
+    <MainComponent msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import MainComponent from "./components/MainComponent.vue";
-window._ = require('lodash');
-window.mc=require('monaco-editor');
+window._ = require("lodash");
+window.mc = require("monaco-editor");
 //window._editor = require('vs/editor/editor.main');
 
 export default {
-  name: "App",
-  components: {
-    MainComponent,
-  },
+    name: "App",
+    components: {
+        MainComponent,
+    },
 };
 </script>
 
@@ -20,19 +20,19 @@ export default {
 
 <style>
 #app {
-  background-color: #333333;
-  height: 100vh;
+    background-color: #333333;
+    height: 100vh;
 }
 
-body{
-  margin: 0px;
+body {
+    margin: 0px;
 }
 
-.newLine{
-  background: green !important;
+.newLine {
+    background: green !important;
 }
 
-.missingLine{
-  background: red !important;
+.missingLine {
+    background: rgba(255, 0, 0, 0.5) !important;
 }
 </style>
